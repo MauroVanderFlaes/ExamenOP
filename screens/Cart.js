5397243226e1dffedac0737ba67639d8741fb4a5
MauroVanderFlaes <85631086+MauroVanderFlaes@users.noreply.github.com>
2023-01-06 01:40:06 +0100

    logo toegevoegd in winkelmandje

--- screens/Cart.js
+++ screens/Cart.js
@@ -15,6 +15,7 @@ const Cart = ({navigation, route}) => {
 
     return (
         <View style={styles.cart}>
+            <Image style={styles.logo} source={require("../assets/logo.png")} />
             <Text style={styles.Title}>Winkelmandje</Text>
             <Text style={styles.text}>Scan deze QR-code om uw producten te betalen.</Text>
             <QRCode style={styles.code} value='https://maurovdf.be/home/index.php/meta/' size={200} color='#0080FB'/>
@@ -24,6 +25,13 @@ const Cart = ({navigation, route}) => {
 }
 
 const styles = StyleSheet.create({
+    logo: {
+        width: 130,
+        height: 26,
+        marginBottom: 20,
+        alignSelf: "center",
+      },
+
     Title: {
         fontSize: 25,
         textAlign: "center",
